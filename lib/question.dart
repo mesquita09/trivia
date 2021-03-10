426fda181f5e584e250f07ffe57519469e929ccb
Adelson <mesquita.adelson@gmail.com>
2021-03-10 20:19:17 -0300

    question list class creation

---
+++ lib/question.dart
@@ -0,0 +1,27 @@
+class Question {
+  String questionText;
+  String option1;
+  String option2;
+  String option3;
+  String option4;
+  int answer;
+
+  Question(this.questionText, this.option1, this.option2, this.option3,
+      this.option4, this.answer);
+
+  static List<Question> getQuestionList() {
+    List<Question> questions = [];
+
+    questions.add(
+      Question(
+        'O que é Flutter',
+        'Novo smartphone',
+        'Um SO',
+        'SDK de desenvolvimento multiplataforma',
+        'Serviço google',
+        3,
+      ),
+    );
+    return questions;
+  }
+}
